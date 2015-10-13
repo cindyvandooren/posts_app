c0c41f7cc98cf7fc2a252e1de16fbe202576cebf
Cindy Van Dooren <cindy.vandooren@gmail.com>
2015-10-13 10:46:43 -0700

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Posts App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Posts App"
   end
 
+  test "should get about" do 
+  	get :about
+  	assert_response :success
+  	assert_select "title", "About | Posts App"
+  end
 end
