00fb6939fed2b49a0958bca36e0557a1dfe61647
Cindy Van Dooren <cindy.vandooren@gmail.com>
2015-10-15 10:44:25 -0700

    Added tests for error validation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -9,6 +9,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 
   test "valid signup information" do
@@ -21,6 +23,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                           }
     end
     assert_template 'users/show'
+    assert_not flash.nil?
   end
 end
+
 
